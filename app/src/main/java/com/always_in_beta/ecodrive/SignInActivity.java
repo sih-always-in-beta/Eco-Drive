8befa068097929834dcffeb63dfa0940cb411b85
mythio <mythio.2909@gmail.com>
2019-01-31 17:38:41 +0530

    Auth fixes and data checks

--- app/src/main/java/com/always_in_beta/ecodrive/SignInActivity.java
+++ app/src/main/java/com/always_in_beta/ecodrive/SignInActivity.java
@@ -3,6 +3,7 @@ package com.always_in_beta.ecodrive;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -16,12 +17,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Objects;
+
 public class SignInActivity extends AppCompatActivity {
 
     private EditText mEditTextEmail;
     private EditText mEditTextPassword;
     private Button mButtonSignIn;
     private TextView mTextViewSignUp;
+
+    private TextInputLayout tx1;
+    private TextInputLayout tx2;
 
     private FirebaseAuth mAuth;
 
@@ -34,16 +40,35 @@ public class SignInActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        mEditTextEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx1.setError(null);
+            }
+        });
+
+        mEditTextPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx2.setError(null);
+            }
+        });
+
         mButtonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEditTextEmail.getText().toString();
                 String pass = mEditTextPassword.getText().toString();
 
-                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)) {
-                    Toast.makeText(SignInActivity.this, "The fields can't be empty!", Toast.LENGTH_SHORT).show();
-                } else {
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)) {
                     signIn(email, pass);
+                } else {
+                    if (TextUtils.isEmpty(email)) {
+                        tx1.setError("Please enter Email ID");
+                    }
+                    if (TextUtils.isEmpty(pass)) {
+                        tx2.setError("Please enter password");
+                    }
                 }
             }
         });
@@ -62,6 +87,10 @@ public class SignInActivity extends AppCompatActivity {
         mEditTextPassword = findViewById(R.id.edit_text_password);
         mButtonSignIn = findViewById(R.id.button_sign_in);
         mTextViewSignUp = findViewById(R.id.text_view_sign_up);
+
+//        Text Input Layouts
+        tx1 = findViewById(R.id.textInputLayout1);
+        tx2 = findViewById(R.id.textInputLayout2);
     }
 
     private void signIn(String email, String pass) {
@@ -69,9 +98,13 @@ public class SignInActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Toast.makeText(SignInActivity.this, "Welcome Back!", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(SignInActivity.this, MainActivity.class));
-                        finish();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignInActivity.this, "Welcome Back!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(SignInActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
     }
--- app/src/main/java/com/always_in_beta/ecodrive/SignUpActivity.java
+++ app/src/main/java/com/always_in_beta/ecodrive/SignUpActivity.java
@@ -1,11 +1,15 @@
 package com.always_in_beta.ecodrive;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,14 +21,25 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText mEditTextName;
     private EditText mEditTextEmail;
     private EditText mEditTextPhone;
     private EditText mEditTextPassword;
+    private EditText mEditTextPasswordConf;
     private Button mButtonSignUp;
     private TextView mTextViewSignIn;
+
+    private TextInputLayout tx1;
+    private TextInputLayout tx2;
+    private TextInputLayout tx3;
+    private TextInputLayout tx4;
+    private TextInputLayout tx5;
 
     private FirebaseAuth mAuth;
 
@@ -37,6 +52,41 @@ public class SignUpActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        mEditTextName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx1.setError(null);
+            }
+        });
+
+        mEditTextEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx2.setError(null);
+            }
+        });
+
+        mEditTextPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx3.setError(null);
+            }
+        });
+
+        mEditTextPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx4.setError(null);
+            }
+        });
+
+        mEditTextPasswordConf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx5.setError(null);
+            }
+        });
+
         mButtonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,11 +94,30 @@ public class SignUpActivity extends AppCompatActivity {
                 String email = mEditTextEmail.getText().toString();
                 String phone = mEditTextPhone.getText().toString();
                 String pass = mEditTextPassword.getText().toString();
+                String passConf = mEditTextPasswordConf.getText().toString();
 
-                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(phone) || TextUtils.isEmpty(pass)) {
-                    Toast.makeText(SignUpActivity.this, "The fields can't be empty!", Toast.LENGTH_SHORT).show();
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(phone) && !TextUtils.isEmpty(pass)) {
+                    if (pass.equals(passConf)) {
+                        signUp(name, email, phone, pass);
+                    } else {
+                        tx5.setError("Passwords don't match");
+                    }
                 } else {
-                    signUp(name, email, phone, pass);
+                    if (TextUtils.isEmpty(name)) {
+                        tx1.setError("Please enter name");
+                    }
+                    if (TextUtils.isEmpty(email)) {
+                        tx2.setError("Please enter Email ID");
+                    }
+                    if (TextUtils.isEmpty(phone)) {
+                        tx3.setError("Please enter phone number");
+                    }
+                    if (TextUtils.isEmpty(pass)) {
+                        tx4.setError("Please enter password");
+                    }
+                    if (TextUtils.isEmpty(passConf)) {
+                        tx5.setError("Please enter password");
+                    }
                 }
             }
         });
@@ -67,8 +136,16 @@ public class SignUpActivity extends AppCompatActivity {
         mEditTextEmail = findViewById(R.id.edit_text_email);
         mEditTextPhone = findViewById(R.id.edit_text_phone);
         mEditTextPassword = findViewById(R.id.edit_text_password);
+        mEditTextPasswordConf = findViewById(R.id.edit_text_password_confirm);
         mButtonSignUp = findViewById(R.id.button_sign_up);
         mTextViewSignIn = findViewById(R.id.text_view_sign_in);
+
+//        Text Input Layouts
+        tx1 = findViewById(R.id.textInputLayout1);
+        tx2 = findViewById(R.id.textInputLayout2);
+        tx3 = findViewById(R.id.textInputLayout3);
+        tx4 = findViewById(R.id.textInputLayout4);
+        tx5 = findViewById(R.id.textInputLayout5);
     }
 
     private void signUp(String name, String email, String phone, String pass) {
@@ -76,17 +153,20 @@ public class SignUpActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            AsyncTask.execute(new Runnable() {
+                                @Override
+                                public void run() {
 
-                        AsyncTask.execute(new Runnable() {
-                            @Override
-                            public void run() {
+                                }
+                            });
 
-                            }
-                        });
-
-                        Toast.makeText(SignUpActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(SignUpActivity.this, MainActivity.class));
-                        finish();
+                            Toast.makeText(SignUpActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
     }
